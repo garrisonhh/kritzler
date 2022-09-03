4d17be31f58f83845891e6212aa528ba9ba8e082
garrisonhh <garrisonhh@pm.me>
2022-09-03 14:14:02 -0400

    use usingnamespace for decl forwards

--- kritzler.zig
+++ kritzler.zig
@@ -1,8 +1,5 @@
 //! exports for kritzler lib, for ease of adding to build files
 
 pub const Color = @import("src/color.zig");
-pub const Canvas = @import("src/canvas.zig").Canvas;
-
-pub const forms = @import("src/forms.zig");
-pub const Table = forms.Table;
-pub const List = forms.List;
+usingnamespace @import("src/canvas.zig");
+usingnamespace @import("src/forms.zig");
