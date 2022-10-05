8adecd0cf3c0e17a540494f1d7bf3af0fd41b987
garrisonhh <garrisonhh@pm.me>
2022-10-05 17:35:34 -0400

    blitting debugging, textures from text

--- src/texture.zig
+++ src/texture.zig
@@ -7,6 +7,7 @@ const Format = @import("format.zig").Format;
 const Allocator = std.mem.Allocator;
 const Pos = types.Pos;
 const Offset = types.Offset;
+const Rect = types.Rect;
 
 const Self = @This();
 
@@ -34,28 +35,109 @@ pub fn init(ally: Allocator, size: types.Pos) Allocator.Error!Self {
     };
 }
 
+pub fn from(
+    ally: Allocator,
+    fmt: Format,
+    text: []const u8
+) Allocator.Error!Self {
+    // count text lines
+    var num_lines: usize = 0;
+    var max_len: usize = 0;
+
+    var lines = std.mem.split(u8, text, "\n");
+    while (lines.next()) |line| {
+        num_lines += 1;
+        max_len = std.math.max(max_len, line.len);
+    }
+
+    // allocate + write lines
+    var tex = try Self.init(ally, Pos{max_len, num_lines});
+    tex.fill(fmt, ' ');
+
+    lines = std.mem.split(u8, text, "\n");
+    var y: usize = 0;
+    while (lines.next()) |line| : (y += 1) {
+        for (line) |ch, x| {
+            tex.buf[tex.indexOf(Pos{x, y})].char = ch;
+        }
+    }
+
+    return tex;
+}
+
 pub fn deinit(self: Self, ally: Allocator) void {
     ally.free(self.buf);
+}
+
+pub fn clone(self: Self, ally: Allocator,) Allocator.Error!Self {
+    return Self{
+        .buf = try ally.dupe(Cell, self.buf),
+        .size = self.size,
+    };
 }
 
 fn indexOf(self: Self, pos: Pos) usize {
     return pos[1] * self.size[0] + pos[0];
 }
 
-pub fn blit(self: *Self, tex: *const Self, to: Pos) void {
-    const allowed_len = std.math.min(self.size[0] - to[0], tex.size[0]);
+/// draw a texture onto this one. in order to avoid allocations, any bits of
+/// the old texture which go out of bounds are ignored.
+pub fn blit(self: *Self, tex: *const Self, to: Offset) void {
+    // find texture intersection
+    const target = Rect{
+        .offset = to,
+        .size = tex.size
+    };
+    const isect = target.intersectionWith(Rect{
+        .offset = .{0, 0},
+        .size = self.size
+    }) orelse {
+        // no intersection found, no blitting required!
+        return;
+    };
 
+    const pos = types.toPos(isect.offset) catch unreachable;
+    const row_len = isect.size[0];
+
+    // copy rows
     var rows = tex.rowIterator();
     var i: usize = 0;
     while (rows.next()) |row| : (i += 1) {
-        const pos = to + Pos{0, i};
-        if (pos[1] >= self.size[1]) break;
+        if (pos[1] + i >= @intCast(usize, isect.offset[1]) + isect.size[1]) {
+            break;
+        }
 
-        const start = self.indexOf(pos);
-        const dest = self.buf[start..start + allowed_len];
+        const start = self.indexOf(pos + Pos{0, i});
+        const dest = self.buf[start..start + row_len];
 
-        std.mem.copy(Cell, dest, row[0..allowed_len]);
+        std.mem.copy(Cell, dest, row[0..row_len]);
     }
+}
+
+/// creates a new texture by drawing two textures on top of each other
+pub fn drawStacked(
+    self: Self,
+    ally: Allocator,
+    tex: *const Self,
+    to: Offset
+) Allocator.Error!Self {
+    // find size of new tex
+    const target = Rect{
+        .offset = to,
+        .size = tex.size
+    };
+    const unified = target.unionWith(Rect{
+        .offset = .{0, 0},
+        .size = self.size
+    });
+
+    // blit two textures onto stacked tex
+    var stacked = try Self.init(ally, unified.size);
+
+    stacked.blit(&self, -unified.offset);
+    stacked.blit(tex, to - unified.offset);
+
+    return stacked;
 }
 
 pub fn fill(self: *Self, fmt: Format, char: u8) void {
--- src/types.zig
+++ src/types.zig
@@ -1,2 +1,60 @@
+//! geometric primitives
+
+const std = @import("std");
+
 pub const Pos = @Vector(2, usize);
 pub const Offset = @Vector(2, isize);
+
+pub const OverflowError = error { Overflow };
+
+pub fn toOffset(pos: Pos) Offset {
+    return Offset{
+        @intCast(isize, pos[0]),
+        @intCast(isize, pos[1])
+    };
+}
+
+pub fn toPos(offset: Offset) OverflowError!Pos {
+    return Pos{
+        try std.math.cast(usize, offset[0]),
+        try std.math.cast(usize, offset[1])
+    };
+}
+
+pub const Rect = struct {
+    const Self = @This();
+
+    offset: Offset,
+    size: Pos,
+
+    pub fn intersectionWith(self: Self, other: Self) ?Self {
+        const offset = @maximum(self.offset, other.offset);
+
+        const size_self = self.offset + toOffset(self.size) - offset;
+        const size_other = other.offset + toOffset(other.size) - offset;
+        const isect_size = @minimum(size_self, size_other);
+
+        // find intersection
+        if (isect_size[0] < 0 or isect_size[1] < 0) {
+            return null;
+        }
+
+        return Self{
+            .offset = offset,
+            .size = toPos(isect_size) catch unreachable,
+        };
+    }
+
+    pub fn unionWith(self: Self, other: Self) Self {
+        const offset = @minimum(self.offset, other.offset);
+
+        const size_self = self.offset + toOffset(self.size) - offset;
+        const size_other = other.offset + toOffset(other.size) - offset;
+        const union_size = @maximum(size_self, size_other);
+
+        return Self{
+            .offset = offset,
+            .size = toPos(union_size) catch unreachable,
+        };
+    }
+};
