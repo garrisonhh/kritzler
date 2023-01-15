033be39f631972dac396240963a1b33007f63161
garrisonhh <garrisonhh@pm.me>
2023-01-14 19:22:16 -0500

    examples for kz.display

--- examples.zig
+++ examples.zig
@@ -6,7 +6,10 @@ const kz = @import("kritzler.zig");
 const Error = Allocator.Error || @TypeOf(stdout).Error;
 
 const Examples = struct {
-    fn hello(ctx: *kz.Context) Error!void {
+    pub fn hello(ally: Allocator) Error!void {
+        var ctx = kz.Context.init(ally);
+        defer ctx.deinit();
+
         const hello_tex = try ctx.print(.{ .fg = .yellow }, "hello", .{});
         const kz_tex = try ctx.print(.{ .fg = .red }, "{s}", .{"kritzler"});
         const tex = try ctx.slap(hello_tex, kz_tex, .right, .{ .space = 1 });
@@ -14,7 +17,10 @@ const Examples = struct {
         try ctx.write(tex, stdout);
     }
 
-    fn todo_list(ctx: *kz.Context) Error!void {
+    pub fn todo_list(ally: Allocator) Error!void {
+        var ctx = kz.Context.init(ally);
+        defer ctx.deinit();
+
         const items = [_][]const u8{
             "rewrite kritzler",
             "promote kritzler",
@@ -45,21 +51,114 @@ const Examples = struct {
 
         try ctx.write(final_tex, stdout);
     }
+
+    /// a realistic struct type
+    const Vec2 = struct {
+        const Self = @This();
+
+        x: f64,
+        y: f64,
+
+        fn of(x: f64, y: f64) Self {
+            return Self{ .x = x, .y = y };
+        }
+
+        /// Vec2 doesn't require any more context to render itself
+        pub fn render(self: Self, ctx: *kz.Context, _: void) !kz.Ref {
+            return try ctx.print(.{}, "({d:2.4}, {d:2.4})", .{self.x, self.y});
+        }
+    };
+
+    pub fn simple_interface(ally: Allocator) Error!void {
+        try kz.display(ally, {}, Vec2.of(1.4, -4.5), stdout);
+    }
+
+    const Monster = struct {
+        const Tag = enum {
+            goblin,
+            spider,
+            human,
+        };
+
+        tag: Tag,
+        pos: Vec2,
+    };
+
+    /// a handle table for monsters
+    const Monsters = struct {
+        const Self = @This();
+
+        data: std.ArrayListUnmanaged(Monster) = .{},
+
+        fn deinit(self: *Self, ally: Allocator) void {
+            self.data.deinit(ally);
+        }
+
+        fn add(self: *Self, ally: Allocator, monster: Monster) Allocator.Error!MonsterId {
+            const id = MonsterId{ .index = self.data.items.len };
+            try self.data.append(ally, monster);
+            return id;
+        }
+
+        fn get(self: Self, id: MonsterId) Monster {
+            return self.data.items[id.index];
+        }
+    };
+
+    /// a handle into the monsters handle table
+    const MonsterId = struct {
+        index: usize,
+
+        pub fn render(self: @This(), ctx: *kz.Context, table: Monsters) !kz.Ref {
+            const m = table.get(self);
+            const tag = try ctx.print(.{ .fg = .red }, "{s}", .{@tagName(m.tag)});
+            const pos = try m.pos.render(ctx, {});
+            return try ctx.slap(tag, pos, .right, .{ .space = 1 });
+        }
+    };
+
+    pub fn complex_interface(ally: Allocator) Error!void {
+        var prng = std.rand.DefaultPrng.init(0);
+        const random = prng.random();
+
+        var table = Monsters{};
+        defer table.deinit(ally);
+
+        // generate a bunch of monsters
+        const N = 100;
+        var ids: [N]MonsterId = undefined;
+
+        var i: usize = 0;
+        while (i < N) : (i += 1) {
+            ids[i] = try table.add(ally, Monster{
+                .tag = random.enumValue(Monster.Tag),
+                .pos = Vec2.of(random.float(f64) * 10, random.float(f64) * 10)
+            });
+        }
+
+        // display a few of my favorite monsters
+        i = 0;
+        while (i < 5) : (i += 1) {
+            const fav = ids[random.intRangeLessThan(usize, 0, N)];
+
+            // in english, this is something like "using the provided allocator
+            // and table, display my favorite monster through stdout."
+            try kz.display(ally, table, fav, stdout);
+        }
+    }
 };
 
 pub fn main() Error!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
-    var ctx = kz.Context.init(gpa.allocator());
-    defer ctx.deinit();
+    const ally = gpa.allocator();
 
     // this just calls each function in examples
     inline for (@typeInfo(Examples).Struct.decls) |decl| {
-        try stdout.print("[{s}]\n", .{decl.name});
-        try @field(Examples, decl.name)(&ctx);
-        try stdout.writeByte('\n');
-
-        std.debug.assert(ctx.numActiveRefs() == 0);
+        if (decl.is_pub) {
+            try stdout.print("[{s}]\n", .{decl.name});
+            try @field(Examples, decl.name)(ally);
+            try stdout.writeByte('\n');
+        }
     }
 }
